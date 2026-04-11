4763ff058b20c734175faedb4a982967cf28d0d5
owen <owenoconnell58@gmail.com>
2026-04-11 15:32:09 -0400

    edited files

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'https://oconneop-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = []
 
 REST_FRAMEWORK = {
