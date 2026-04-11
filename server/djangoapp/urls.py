cb3456b0997e1c14df035531b0c05a733e50fbf2
owen <owen>
2026-04-11 19:48:02 -0400

    edited files

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,17 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
-
+from . import views
+from django.views.generic import TemplateView
+from django.urls import path, include
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
+    path(route='login', view=views.login_user, name='login'),
+    # path for dealer reviews view 
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
