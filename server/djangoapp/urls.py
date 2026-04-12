960053d32ac41737edc99edfd68ab2610481328c
owen <owen>
2026-04-11 21:21:22 -0400

    edited files

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path(route='registration', view=views.registration, name='registration'),
     # path for login
     path(route='login', view=views.login_user, name='login'),
+    
+    path(route='logout', view=views.logout_request, name='logout'),
     # path for dealer reviews view 
     # path for add a review view
 
