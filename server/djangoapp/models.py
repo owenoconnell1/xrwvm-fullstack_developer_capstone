a0ed5d85d51cc85ed55bd689c9dc467fa6e6f1c0
owen <owen>
2026-04-13 03:12:20 -0400

    Edited models.py, populate.py, urls.py, views.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -15,7 +15,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    def __str__():
+    def __str__(self):
         return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -28,9 +28,10 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
+    car_make = ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     car_types=[("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon")]
     type = models.CharField(max_length=10, choices=car_types, default="SUV")
     year = models.IntegerField(default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)])
-    def __str__():
+    def __str__(self):
         return self.name
