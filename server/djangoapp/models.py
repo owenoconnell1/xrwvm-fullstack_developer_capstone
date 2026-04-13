9996bf2fe4058cb7f3cfd76b54afbeccec7cf28b
owen <owen>
2026-04-13 16:32:23 -0400

    edited models.py and settings.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -29,7 +29,6 @@ class CarMake(models.Model):
 # - __str__ method to print a car make object
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    
     name = models.CharField(max_length=100)
     car_types=[("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon")]
     type = models.CharField(max_length=10, choices=car_types, default="SUV")
