352638bd842a307f0aaaf0b8a6851441495e0bdb
Owen <owenoconnell>
2026-04-14 14:05:02 -0400

    added trailing slash to route for get dealers by state

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     # path for get_dealers by ID
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # path for get_dealers by state
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='get_dealers/<str:state>/', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for add a review view
     path(route='add_review', view=views.add_review, name ='add_review'),
     # path for reviews by ID
