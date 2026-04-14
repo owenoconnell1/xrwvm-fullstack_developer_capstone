3368f4b9b6ad7a95f6c56e6c72860897490163d8
Your Name <you@example.com>
2026-04-14 01:35:13 -0400

    added route to app.js, fixed bug in urls.py, fixed backend url in .env

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,11 +16,11 @@ urlpatterns = [
     # path for dealer reviews view
     
     # path for get_dealers
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    # path for get_dealers by state
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     # path for get_dealers by ID
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    # path for get_dealers by state
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for add a review view
     path(route='add_review', view=views.add_review, name ='add_review'),
     # path for reviews by ID
