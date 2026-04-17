e3a48dd735bea05e9c71f4301cf173812bbea572
Your Name <you@example.com>
2026-04-17 02:09:45 -0400

    Fixed spacing in models.py and populate.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -46,8 +46,10 @@ class CarModel(models.Model):
     )
     year = models.IntegerField(
         default=2023,
-        validators=[MaxValueValidator(2023),
-        MinValueValidator(2015)]
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
     )
 
     def __str__(self):
